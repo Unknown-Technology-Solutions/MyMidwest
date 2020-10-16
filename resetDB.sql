a25d5326f97fded790c62a3635173ed4905c7c61
TotallyAProgrammer <zane.reick@unknownts.tk>
2020-10-16 06:37:38 +0000

    Added table for users

--- resetDB.sql
+++ resetDB.sql
@@ -1,6 +1,21 @@
 -- Zane Reick, My Midwest reset testing Database to default values.
 
--- IF DATABASE EXISTS (USE DATABASE);
+-- Create Database if it doesn't exist
 CREATE DATABASE IF NOT EXISTS MyMidwest_v1;
 
-DROP
+USE MyMidwest_v1;
+
+-- Drop old tables, and create new ones
+
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS posts;
+
+CREATE TABLE users (
+	uid        INT UNSIGNED AUTO_INCREMENT NOT NULL UNIQUE KEY PRIMARY KEY,
+	username   VARCHAR(255) NOT NULL UNIQUE,
+	password   VARCHAR(255) NOT NULL,
+	firstName  VARCHAR(255) NOT NULL,
+	lastName   VARCHAR(255) NOT NULL,
+	company    VARCHAR(255) DEFAULT NULL
+);
+
