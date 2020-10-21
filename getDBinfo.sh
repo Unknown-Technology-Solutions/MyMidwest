ae67192e35fe0157168ce0854b33a30cf4692d6f
Zane D. R <kyle.unknown@outlook.com>
2020-10-21 11:26:46 -0500

    Fixed JSON formating error

--- getDBinfo.sh
+++ getDBinfo.sh
@@ -4,9 +4,9 @@ CONFIG_DIR=/var/www/testing/data
 
 # Create Database configuration
 echo "{" >> $CONFIG_DIR/DBconfig.json
-echo "\"database\": \"$MSQL_DB\"" >> $CONFIG_DIR/DBconfig.json
-echo "\"username\": \"$MSQL_UNAME\"" >> $CONFIG_DIR/DBconfig.json
-echo "\"password\": \"$MSQL_PASS\"" >> $CONFIG_DIR/DBconfig.json
-echo "\"server\": \"$MSQL_SERVER\"" >> $CONFIG_DIR/DBconfig.json
+echo "\"database\": \"$MSQL_DB\"," >> $CONFIG_DIR/DBconfig.json
+echo "\"username\": \"$MSQL_UNAME\"," >> $CONFIG_DIR/DBconfig.json
+echo "\"password\": \"$MSQL_PASS\"," >> $CONFIG_DIR/DBconfig.json
+echo "\"server\": \"$MSQL_SERVER\"," >> $CONFIG_DIR/DBconfig.json
 echo "\"port\": \"$MSQL_PORT\"" >> $CONFIG_DIR/DBconfig.json
 echo "}" >> $CONFIG_DIR/DBconfig.json
